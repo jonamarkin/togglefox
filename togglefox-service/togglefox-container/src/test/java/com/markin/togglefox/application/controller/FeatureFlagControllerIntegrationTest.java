f5a2f124d7765dea036c6a4ac2ab706593d19b78
Jonathan Ato Markin <jona10711@gmail.com>
2025-06-02 22:12:59 +0200

    Redis issue

--- togglefox-service/togglefox-container/src/test/java/com/markin/togglefox/application/controller/FeatureFlagControllerIntegrationTest.java
+++ togglefox-service/togglefox-container/src/test/java/com/markin/togglefox/application/controller/FeatureFlagControllerIntegrationTest.java
@@ -9,7 +9,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
